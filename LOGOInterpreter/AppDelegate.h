c68474a3f8433b9eb72f94fea55c7efe9b4c0ac0
Chengpeng Qin <cpqin@Chengpengs-Mac.local>
2015-03-22 22:16:00 -0700

    Initial Commit

---
+++ LOGOInterpreter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LOGOInterpreter
+//
+//  Created by Chengpeng Qin on 22/3/15.
+//  Copyright (c) 2015 Chengpeng Qin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LOGOInterpreter/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  LOGOInterpreter
+//
+//  Created by Chengpeng Qin on 22/3/15.
+//  Copyright (c) 2015 Chengpeng Qin. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
